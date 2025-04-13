77e0bf176dd2d3f9c5685b254a39bd54f48ba040
brunohro <brunoh.rodrigues27@gmail.com>
2025-04-13 15:49:08 -0300

    Adicionando a função remover

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import AddTask from "./components/AddTask";
+// import AddTask from "./components/AddTask";
 import Task from "./components/Task";
 
 function App() {
@@ -23,12 +23,32 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = task.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+    setTask(newTasks); // passando a nova lista atualizada
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = task.filter((t) => t.id != taskId);
+    setTask(newTasks);
+  }
   return (
     <div className="w-screen h-screen bg-slate-500 justify-center p-6">
       <h1 className="text-3xl text-slate-100 font-bold text-center">
         Gerenciador de Tarefas
       </h1>
-      <Task tasks={task} />
+      <Task
+        tasks={task}
+        onTaskClick={onTaskClick}
+        onDeleteTaskClick={onDeleteTaskClick}
+      />
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,26 @@
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
 function Task(props) {
   return (
-    <ul className="space-y-4">
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
-        <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">
-          {" "}
-          {task.title}
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-white text-left p-2 rounded-md 
+              ${task.isCompleted && "line-through"} `}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 text-white p-2 rounded-md">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => props.onDeleteTaskClick(task.id)}
+            className="bg-slate-400 text-white p-2 rounded-md"
+          >
+            <TrashIcon />
+          </button>
         </li>
       ))}
     </ul>
