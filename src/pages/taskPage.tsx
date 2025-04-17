810694971154fb0f47e615ca9320e1200ab0c016
brunohro <brunoh.rodrigues27@gmail.com>
2025-04-17 09:46:58 -0300

    Estilizando página de detalhar tarefa

--- src/pages/taskPage.tsx
+++ src/pages/taskPage.tsx
@@ -1,14 +1,28 @@
+import { ChevronLeftIcon } from "lucide-react";
 import React from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
   return (
-    <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <h1>{description}</h1>
+    <div className="h-screen w-screen bg-slate-500 p-6 flex justify-center">
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center mb-6 gap-8">
+          <button onClick={() => navigate(-1)} className="text-white">
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da tarefa
+          </h1>
+        </div>
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl text-slate-600 font-bold">{title}</h2>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
