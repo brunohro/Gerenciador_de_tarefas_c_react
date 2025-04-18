5bd7c4869d7e1d921598e8d2658d7ef530c1a5d2
brunohro <brunoh.rodrigues27@gmail.com>
2025-04-18 11:41:25 -0300

    Adicionando um CheckIcon ao concluir a tarefa

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 import React from "react";
@@ -19,9 +19,10 @@ function Task(props) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => props.onTaskClick(task.id)}
-            className={`bg-slate-400 text-white text-left p-2 rounded-md 
+            className={`bg-slate-400 flex text-white text-left p-2 rounded-md 
               ${task.isCompleted && "line-through"} `}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <Button onClick={() => onSeeDetailsClick(task)}>
